14074f34d11aa44b818ec81f7ec42c7dd36e9f35
Scott Davies <scottdavies1983@gmail.com>
2020-12-08 18:58:07 +0000

    started popup cancel function and dom removal

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+async function destroyPopUp(popup) {
+  popup.classList.remove("open");
+  await wait(1000);
+  // remove the popup entirely!
+  popup.remove();
+}
+
 function ask(options) {
   return new Promise(async function (resolve) {
     // First we need to create a popup with all the fields in it
@@ -9,9 +16,10 @@ function ask(options) {
     popup.classList.add("popup");
     popup.insertAdjacentHTML(
       "afterbegin",
-      `
-      <fieldset>
+      `<fieldset>
         <label>${options.title}</label>
+        <input type="text" name="input"/>
+        <button type="submit">Submit</button>
       </fieldset>
     `
     );
@@ -20,10 +28,24 @@ function ask(options) {
     if (options.cancel) {
       const skipButton = document.createElement("button");
       skipButton.type = "button";
-      skipButton.textContent = "cancel";
+      skipButton.textContent = "Cancel";
+      popup.firstElementChild.appendChild(skipButton);
       // TODO: listen for a click on the cancel button
+      popup.addEventListener(
+        "submit",
+        function (e) {
+          e.preventDefault();
+          resolve(e.target.input.value);
+          // remove it from the DOM entireley
+          destroyPopUp(popup);
+        },
+        { once: true }
+      );
     }
     // listen for the submit event on the inputs
+
+    // remove the eveny listener for submitted
+
     // when someone does sublmit it, resolve the data that was in the input box
 
     // insert that popup into the dom
