93484bdeb7ad422287d0fef47d33a30a6643f38f
Scott Davies <scottdavies1983@gmail.com>
2020-12-09 06:38:01 +0000

    quetsion prompt stream

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ async function destroyPopUp(popup) {
   await wait(1000);
   // remove the popup entirely!
   popup.remove();
+  // eslint-disable-next-line no-param-reassign
+  popup = null;
 }
 
 function ask(options) {
@@ -31,19 +33,26 @@ function ask(options) {
       skipButton.textContent = "Cancel";
       popup.firstElementChild.appendChild(skipButton);
       // TODO: listen for a click on the cancel button
-      popup.addEventListener(
-        "submit",
-        function (e) {
-          e.preventDefault();
-          resolve(e.target.input.value);
-          // remove it from the DOM entireley
+      skipButton.addEventListener(
+        "click",
+        function () {
+          resolve(null);
           destroyPopUp(popup);
         },
         { once: true }
       );
     }
     // listen for the submit event on the inputs
-
+    popup.addEventListener(
+      "submit",
+      function (e) {
+        e.preventDefault();
+        resolve(e.target.input.value);
+        // remove it from the DOM entireley
+        destroyPopUp(popup);
+      },
+      { once: true }
+    );
     // remove the eveny listener for submitted
 
     // when someone does sublmit it, resolve the data that was in the input box
@@ -60,3 +69,56 @@ function ask(options) {
     popup.classList.add("open");
   });
 }
+
+// select all buttons that have a question
+async function askQuestion(e) {
+  const button = e.currentTarget;
+  const shouldCancel = "cancel" in button.dataset; // can also use has attribute
+  const answer = await ask({
+    title: button.dataset.question,
+    cancel: shouldCancel,
+  });
+  console.log(answer);
+}
+
+const buttons = document.querySelectorAll("[data-question");
+buttons.forEach((button) => button.addEventListener("click", askQuestion));
+
+// The below creates an example of a stream of questions
+
+const questions = [
+  { title: "What is your name?" },
+  { title: "What is your age?", cancel: true },
+  { title: "What is your dogs name?" },
+];
+
+// utility function
+async function asyncMap(array, callback) {
+  // make an array and store our results
+  const results = [];
+  // loop over our array
+  for (const item of array) {
+    const result = await callback(item);
+    results.push(result);
+  }
+  // when the loop is done, return it!
+  return results;
+}
+
+async function go() {
+  const answers = await asyncMap(questions, ask);
+  console.log(answers);
+}
+
+go();
+
+// Replaced with above utilty function
+
+// async function askMany() {
+//   for (const question of questions) {
+//     const answer = await ask(question);
+//     console.log(answer);
+//   }
+// }
+
+// askMany();
